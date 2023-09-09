865ef0e1c3da92acbe03bec7bc620eff3e93e845
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-08 23:37:21 -0300

    creacion de componentes header y footer

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <>
+        <h1>Esto es un footer</h1>
+        </>
+    )
+    }
+
+export default Footer
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <>
+        <h1>Esto es un header</h1>
+        </>
+    )
+    }
+
+export default Header
