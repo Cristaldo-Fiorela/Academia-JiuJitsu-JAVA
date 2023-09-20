d467287d218b4d27fb1af404d6ab1767760d727e
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-19 21:22:51 -0300

    configuracion de proyecto con tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <Header />
-        <h1>Proyecto JAVA</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>      
       <Footer />
     </>
   )
