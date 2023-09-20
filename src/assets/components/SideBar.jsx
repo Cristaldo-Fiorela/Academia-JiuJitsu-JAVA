ca86339b50b51cce44cd75afb80ccd67b35891b6
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-20 15:28:16 -0300

    correccion de indentado

--- src/assets/components/SideBar.jsx
+++ src/assets/components/SideBar.jsx
@@ -7,24 +7,22 @@ const SideBar = (props) => {
 
     return (
         <>
-        <div className={`${!open && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 backdrop-blur-sm`} onClick={() => setOpen(false)}></div>
+            <div className={`${!open && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 backdrop-blur-sm`} onClick={() => setOpen(false)}></div>
 
-        <div className={`${open ? 'w-80' : 'w-0'} bg-cyan-600 min-h-screen w-80 fixed top-0 right-0 transition-all duration-300`}>
-            <div className={`${!open && 'hidden'} pt-3`}>
-                <button className='ml-4 mb-10 text-white cursor-pointer' onClick={() => setOpen(false)}>
-                    <FontAwesomeIcon icon={faXmark} className='h-6 w-6 hover:text-gray-200'/>
-                </button>
-                <div>
-                    <ul className="px-4 mx-auto font-semibold flex-col text-2xl">
-                        <li className='py-3 mb-2'><a className="hover:text-gray-200 " href="#">Inicio</a></li>
-                        <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Sobre Nosotros</a></li>
-                        <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Profesores</a></li>
-                        <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Noticias</a></li>
-                        <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Contacto</a></li>
-                    </ul>
+            <aside className={`${open ? 'w-80' : 'w-0'} bg-cyan-600 min-h-screen w-80 fixed top-0 right-0 transition-all duration-300`}>
+                <div className={`${!open && 'hidden'} pt-3`}>
+                    <button className='ml-4 mb-10 text-white cursor-pointer' onClick={() => setOpen(false)}>
+                        <FontAwesomeIcon icon={faXmark} className='h-6 w-6 hover:text-gray-200'/>
+                    </button>
+                        <ul className="px-4 mx-auto font-semibold flex-col text-2xl">
+                            <li className='py-3 mb-2'><a className="hover:text-gray-200 " href="#">Inicio</a></li>
+                            <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Sobre Nosotros</a></li>
+                            <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Profesores</a></li>
+                            <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Noticias</a></li>
+                            <li className='py-3 mb-2'><a className="hover:text-gray-200 py-3 mb-2" href="#">Contacto</a></li>
+                        </ul>
                 </div>
-            </div>
-        </div>
+            </aside>
         </>
     )
 }
