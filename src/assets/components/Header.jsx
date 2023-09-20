167c11cfe8ca37ee6b807634806dcf8eea98d317
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-20 00:03:47 -0300

    correccion de scroll lateral en componente

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -13,39 +13,38 @@ function Header() {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className="flex flex-wrap h-screen">
-        <section className="relative mx-auto">
+        <header className="flex flex-wrap">
+            <section className="relative mx-auto">
                 <nav className="flex justify-between bg-gray-900 text-white w-screen">
-                <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-                <a className="text-3xl font-bold font-heading" href="#">
-                    <h1>
-                        Felino&nbsp;
-                        <FontAwesomeIcon icon={faShieldCat} />
-                        &nbsp;Warriors
-                    </h1>        
-                </a>
+                    <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+                    <a className="text-3xl font-bold font-heading" href="#">
+                        <h1>
+                            Felino&nbsp;
+                            <FontAwesomeIcon icon={faShieldCat} />
+                            &nbsp;Warriors
+                        </h1>        
+                    </a>
 
-                {/* <!-- Nav Links --> */}
+                    {/* <!-- Nav Links --> */}
 
-                <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                    <li><a className="hover:text-gray-200" href="#">Inicio</a></li>
-                    <li><a className="hover:text-gray-200" href="#">Sobre Nosotros</a></li>
-                    <li><a className="hover:text-gray-200" href="#">Profesores</a></li>
-                    <li><a className="hover:text-gray-200" href="#">Noticias</a></li>
-                    <li><a className="hover:text-gray-200" href="#">Contacto</a></li>
-                </ul>
-                {/* <!-- Header Icons --> */}
+                    <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                        <li><a className="hover:text-gray-200" href="#">Inicio</a></li>
+                        <li><a className="hover:text-gray-200" href="#">Sobre Nosotros</a></li>
+                        <li><a className="hover:text-gray-200" href="#">Profesores</a></li>
+                        <li><a className="hover:text-gray-200" href="#">Noticias</a></li>
+                        <li><a className="hover:text-gray-200" href="#">Contacto</a></li>
+                    </ul>
 
-                <div className="hidden xl:flex items-center space-x-5 items-center">
-                {/* <!-- Sign In / Register      --> */}
-                <a className="flex items-center hover:text-gray-200" href="#">
-                    <FontAwesomeIcon icon={faCircleUser} className='h-6 w-6 hover:text-gray-200'/>
-                </a>
+                    <div className="hidden xl:flex items-center space-x-5 items-center">
+                    {/* <!-- Sign In / Register      --> */}
+                    <a className="flex items-center hover:text-gray-200" href="#">
+                        <FontAwesomeIcon icon={faCircleUser} className='h-6 w-6 hover:text-gray-200'/>
+                    </a>
 
-                </div>
-                </div>
-                {/* <!-- Responsive navbar --> */}
-                    
+                    </div>
+                    </div>
+
+                    {/* <!-- Responsive navbar --> */}
                     <button className="navbar-burger self-center mr-12 xl:hidden" onClick={() => setOpen(true)}>
                         <FontAwesomeIcon icon={faBars} className='h-6 w-6 hover:text-gray-200' />
                     </button>
