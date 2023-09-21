6abe6e178cb62e5f07a1ed536c41be564d4f8bf1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-20 23:16:04 -0300

    reemplazo de imagenes

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -1,12 +1,19 @@
+import JiuJitsu from '../images/jiu-jitsu-5641080-1920.png'
+
 function Banner() {
     return (
         <>
-            <section>
-                <h2>Accede a nuestro contenido privado para alumnos y profesores</h2>
-                <div>
-                    <button>Inicia Sesion</button>
-                    <button>Registrate</button>
-                </div>
+            {/* <div className="bg-red-500 h-96 w-screen"></div> */}
+            <section className="w-auto bg-black flex justify-center" id="banner">
+                <img src={ JiuJitsu } alt="" className="" />
+                <section className='bg-red-500'>
+                    <h2>Accede a nuestro contenido privado para alumnos y profesores</h2>
+                    <div>
+                        <button className="bg-red-500">Inicia Sesion</button>
+                        <button>Registrate</button>
+                    </div>
+                </section>
+
             </section>
         </>
     )
