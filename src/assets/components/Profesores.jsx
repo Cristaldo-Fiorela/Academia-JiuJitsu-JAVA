660ab05fe32ee64c59353794c0992f9d9d5b23b1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-21 02:45:45 -0300

    componente de profesores terminado, pendiente responsive

--- src/assets/components/Profesores.jsx
+++ src/assets/components/Profesores.jsx
@@ -5,23 +5,23 @@ import ProfJuan from '../images/bjj-8205246_1280.jpg'
 function Profesores() {
     return (
         <>
-        <section className="text-white  flex flex-col items-center py-10">
-            <h3 className="text-4xl font-bold">Profesores</h3>
-            <div>
-                <section>
-                    <img src={ ProfJuan } alt="" />
-                    <h4>Juan González</h4>
-                </section>
-                <section>
-                    <img src={ ProfMartina } alt="" />
-                    <h4>Martina López</h4>
-                </section>
-                <section>
-                    <img src={ ProfPablo } alt="" />
-                    <h4>Pablo Rodríguez</h4>
-                </section>
-            </div>
-        </section>
+            <section className="text-white  flex flex-col items-center py-10">
+                <h3 className="text-4xl font-bold text-emerald-400 uppercase">Profesores</h3>
+                <div className='flex flex-wrap py-10 justify-center items-center gap-10'>
+                    <section className='flex flex-col items-center gap-5'>
+                        <img src={ ProfJuan } alt="retrato profesor juan" className='h-80 rounded-full shadow-lg shadow-emerald-900 hover:shadow-emerald-500'/>
+                        <h4 className='text-2xl'>Juan González</h4>
+                    </section>
+                    <section className='flex flex-col items-center gap-5'>
+                        <img src={ ProfMartina } alt="retrato profesora martina" className='h-80 rounded-full shadow-lg shadow-emerald-900 hover:shadow-emerald-500' />
+                        <h4 className='text-2xl'>Martina López</h4>
+                    </section>
+                    <section className='flex flex-col items-center gap-5'>
+                        <img src={ ProfPablo } alt="retrato profesor pablo" className='h-80 rounded-full shadow-lg shadow-emerald-900 hover:shadow-emerald-500' />
+                        <h4 className='text-2xl'>Pablo Rodríguez</h4>
+                    </section>
+                </div>
+            </section>
         </>
     )
     }
