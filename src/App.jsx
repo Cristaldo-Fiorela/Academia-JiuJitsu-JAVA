83f5a529801bc7d92119a35af724e1838b314bf1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-21 00:55:04 -0300

    template base de componente "sobre nosotros"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './assets/components/Header';
 import Footer from './assets/components/Footer';
 import Banner from './assets/components/Banner';
+import SobreNosotros from './assets/components/SobreNosotros';
+
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <SobreNosotros />
       <Footer />
     </>
   )
--- src/assets/components/SobreNosotros.jsx
+++ src/assets/components/SobreNosotros.jsx
@@ -1,9 +1,23 @@
+import BJJAcademia from '../images/bjj-8111394_1280.jpg'
+
 function SobreNosotros() {
     return (
         <>
-        <h1>Esto es un about us</h1>
+            <section className='text-white'>
+                <h3 className="">Sobre Nosotros</h3>
+
+                <div>
+                    <img src={ BJJAcademia } alt="hombres en combate de BJJ" />
+                    
+                    <section>
+                        <h3>Superación a través de la Disciplina y la Amistad</h3>
+                        <p>En Felino Warriors, abrazamos la pasión por el Jiu-Jitsu Brasileño. Somos una comunidad de entusiastas comprometidos con el crecimiento personal a través del arte del Jiu-Jitsu.</p>
+                        <p>Nacimos de la idea de crear un lugar donde todos pudieran aprender, crecer y evolucionar juntos, manteniendo siempre la filosofía de respeto, amistad y superación que nos impulsa.</p>
+                    </section>
+                </div>
+            </section>
         </>
     )
     }
 
-export default SobreNosotros
+export default SobreNosotros;
