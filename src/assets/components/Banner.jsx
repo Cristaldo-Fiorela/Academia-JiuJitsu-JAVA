0bb32e3844239e7cdd354a25c0ad06a6683bbde2
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-21 18:24:37 -0300

    responsive 640px

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -7,18 +7,18 @@ import { faShieldCat } from '@fortawesome/free-solid-svg-icons';
 function Banner() {
     return (
         <>
-            <section className="w-auto flex flex-wrap justify-center text-slate-200 py-10" id="banner">
-                <img src={ JiuJitsu } alt="hombre de espaldas con kimono" />
+            <section className="w-auto flex flex-wrap justify-center text-slate-200 py-10 sm:py-0" id="banner">
+                <img src={ JiuJitsu } alt="hombre de espaldas con kimono" className='sm:h-80' />
                 <section className='flex flex-col justify-center items-center '>
                     <h2 className='text-3xl pt-2 lg:text-6xl  font-bold upp'>
                         Felino&nbsp;
                         <FontAwesomeIcon icon={faShieldCat} className='text-emerald-400' />
                         &nbsp;Warriors
                     </h2>
-                    <h3 className='text-center py-5 text-2xl lg:py-10 w-72'>Accede a nuestro contenido privado para alumnos y profesores.</h3>
-                    <div className='flex justify-start gap-10     text-zinc-950 font-bold'>
-                        <button className="bg-emerald-400 py-3 px-4 lg:py-3 lg:px-5 rounded-lg hover:bg-green-400 active:translate-y-1">Inicia Sesión</button>
-                        <button className="bg-emerald-400 py-3 px-4 lg:py-3 lg:px-5 rounded-lg hover:bg-green-400 active:translate-y-1">Registrate</button>                    
+                    <h3 className='text-center py-5 text-2xl sm:text-xl lg:py-10 w-72'>Accede a nuestro contenido privado para alumnos y profesores.</h3>
+                    <div className='flex justify-start gap-10 text-zinc-950 font-bold'>
+                        <button className="bg-emerald-400 py-3 px-4 sm:px-3 lg:py-3 lg:px-5 rounded-lg hover:bg-green-400 active:translate-y-1">Inicia Sesión</button>
+                        <button className="bg-emerald-400 py-3 px-4 sm:px-3 lg:py-3 lg:px-5 rounded-lg hover:bg-green-400 active:translate-y-1">Registrate</button>                    
                     </div>
                 </section>
 
--- src/assets/components/SobreNosotros.jsx
+++ src/assets/components/SobreNosotros.jsx
@@ -7,7 +7,7 @@ function SobreNosotros() {
             <section className='text-white flex flex-col items-center bg-zinc-900 py-10' id='sobreNosotros'>
                 <h3 className="text-2xl lg:text-4xl font-bold uppercase">Sobre Nosotros</h3>
 
-                <div className='flex flex-wrap justify-center gap-16 py-10'>
+                <div className='flex flex-wrap justify-center gap-16 py-10 sm:gap-5'>
                     <section className='flex flex-col text-lg items-center w-72 lg:text-xl lg:w-96 justify-center gap-3'>
                         <h3 className='text-xl' >Superación a través de la <span className='text-emerald-400 font-bold'>Disciplina</span> y la <span className='text-emerald-400 font-bold'>Amistad</span>.</h3>
                         <p>En Felino Warriors, abrazamos la pasión por el Jiu-Jitsu Brasileño. Somos una comunidad de entusiastas comprometidos con el crecimiento personal a través del arte del Jiu-Jitsu.</p>
