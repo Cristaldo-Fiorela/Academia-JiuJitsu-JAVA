39b524d9d39b78b2834f40c0d1f167dded96cacb
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-21 01:55:25 -0300

    estilos de section sobre nosotros agregados, pendiente responsive

--- src/assets/components/SobreNosotros.jsx
+++ src/assets/components/SobreNosotros.jsx
@@ -1,23 +1,28 @@
 import BJJAcademia from '../images/bjj-8111394_1280.jpg'
+import BJJUnion from '../images/hands-1846428_1280.jpg'
 
 function SobreNosotros() {
     return (
         <>
-            <section className='text-white'>
-                <h3 className="">Sobre Nosotros</h3>
+            <section className='text-white flex flex-col items-center     bg-zinc-900 py-10'>
+                <h3 className="text-4xl font-bold">Sobre Nosotros</h3>
 
-                <div>
-                    <img src={ BJJAcademia } alt="hombres en combate de BJJ" />
-                    
-                    <section>
-                        <h3>Superación a través de la Disciplina y la Amistad</h3>
+                <div className='flex flex-wrap justify-center gap-16 py-10'>
+                    <section className='flex flex-col text-xl w-96 justify-center gap-3'>
+                        <h3 className='text-2xl' >Superación a través de la <span className='text-emerald-400 font-bold'>Disciplina</span> y la <span className='text-emerald-400 font-bold'>Amistad</span>.</h3>
                         <p>En Felino Warriors, abrazamos la pasión por el Jiu-Jitsu Brasileño. Somos una comunidad de entusiastas comprometidos con el crecimiento personal a través del arte del Jiu-Jitsu.</p>
                         <p>Nacimos de la idea de crear un lugar donde todos pudieran aprender, crecer y evolucionar juntos, manteniendo siempre la filosofía de respeto, amistad y superación que nos impulsa.</p>
                     </section>
+
+                    <img src={ BJJUnion } alt="hombres en combate de BJJ" className='w-72 rounded-lg filter drop-shadow-xl saturate-50 hover:saturate-100 transition duration-300' />
                 </div>
             </section>
+            <div class="transform h-20 -skew-y-2 origin-bottom-left inset-0 bg-zinc-900">
+            </div>
         </>
     )
     }
 
 export default SobreNosotros;
+
+//     <div class="absolute inset-0 bg-blue-500 clip-triangle"></div>
