9c523ca64ebae3877d133893ae1309d4d3d19552
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-22 22:03:29 -0300

    Estructura de footer inicial

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,8 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShieldCat } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
     return (
         <>
-        <h1>Esto es un footer</h1>
+            <footer>
+                <section>
+                    <h2>Unete a nuestra comunidad!</h2>
+                    <form>
+                        <div>
+                            <label htmlFor="nombre">Nombre:</label>
+                            <input type="text" id="nombre"/>
+                        </div>
+                        <div>
+                            <label htmlFor="email">Correo:</label>
+                            <input type="email" id="email"/>
+                        </div>
+                        <div>
+                            <label htmlFor="telefono">Telefono:</label>
+                            <input type="tel" id="telefono"/>
+                        </div>
+                    </form>
+                </section>
+                <section>
+                    <FontAwesomeIcon icon={faShieldCat} />
+
+                    <h2> Hecho por Cristaldo Fiorela</h2>
+
+                </section>
+            </footer>
         </>
     )
     }
