fa441a026206acb6a479689b3340e4a385f8fd27
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-22 22:27:35 -0300

    estilos de formulario

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -7,24 +7,24 @@ function Footer() {
             <footer className='bg-emerald-400'>
                 <section className='py-10 flex flex-col justify-around items-center h-96'>
                     <h2 className='text-2xl lg:text-4xl font-bold uppercase'>Unete a nuestra comunidad!</h2>
-                    <form>
-                        <div>
-                            <label htmlFor="nombre">Nombre:</label>
-                            <input type="text" id="nombre"/>
+                    <form className='bg-slate-200 h-60 w-72 mt-5 rounded-lg flex flex-wrap justify-center items-center'>
+                        <div className='flex w-full justify-around'>
+                            <label htmlFor="nombre" className='font-bold'>Nombre:</label>
+                            <input type="text" id="nombre" className='rounded-md'/>
                         </div>
-                        <div>
-                            <label htmlFor="email">Correo:</label>
-                            <input type="email" id="email"/>
+                        <div className='flex w-full justify-around'>
+                            <label htmlFor="email" className='font-bold'>Correo:</label>
+                            <input type="email" id="email" className='rounded-md'/>
                         </div>
-                        <div>
-                            <label htmlFor="telefono">Telefono:</label>
-                            <input type="tel" id="telefono"/>
+                        <div className='flex w-full justify-around'>
+                            <label htmlFor="telefono" className='font-bold'>Telefono:</label>
+                            <input type="tel" id="telefono" className='rounded-md'/>
                         </div>
                     </form>
                 </section>
                 <section>
                     <FontAwesomeIcon icon={faShieldCat} />
-                    <h2> Hecho por Cristaldo Fiorela</h2>
+                    <h3>Hecho por Cristaldo Fiorela</h3>
                 </section>
             </footer>
         </>
