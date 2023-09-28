082ae8d0d51643d7c156e573eb330b2533ed5022
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-09-28 19:35:44 -0300

    estructura inicial de seccion de noticias

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Footer from './assets/components/Footer';
 import Banner from './assets/components/Banner';
 import SobreNosotros from './assets/components/SobreNosotros';
 import Profesores  from './assets/components/Profesores';
-
+import Noticias from './assets/components/Noticias';
 
 function App() {
   return (
     <>
       <Header />
-      <Banner />
-      <SobreNosotros />
-      <Profesores />
+        <Banner />
+        <SobreNosotros />
+        <Profesores />
+        <Noticias/>
       <Footer />
     </>
   )
--- src/assets/components/Noticias.jsx
+++ src/assets/components/Noticias.jsx
@@ -1,7 +1,29 @@
 function Noticias() {
     return (
         <>
-        <h1>Esto es un noticias</h1>
+            <section className="text-white">
+                <h3>Noticias y eventos</h3>
+                <div>
+                    <article>
+                        <h6>13 septiembre 2023</h6>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum autem dolorum, repellendus earum officia expedita itaque delectus culpa maxime placeat voluptatum necessitatibus!</p>
+                        <button>Leer mas..</button>
+                    </article>
+                    <article>
+                        <h6>13 septiembre 2023</h6>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum autem dolorum, repellendus earum officia expedita itaque delectus culpa maxime placeat voluptatum necessitatibus!</p>
+                        <button>Leer mas..</button>
+                    </article>
+                    <article>
+                        <h6>13 septiembre 2023</h6>
+                        <h4>Lorem ipsum dolor sit.</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum autem dolorum, repellendus earum officia expedita itaque delectus culpa maxime placeat voluptatum necessitatibus!</p>
+                        <button>Leer mas..</button>
+                    </article>
+                </div>
+            </section>
         </>
     )
     }
