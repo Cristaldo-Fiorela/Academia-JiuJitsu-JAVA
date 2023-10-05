be87421d51b11fe6fd31415d4bdf12acf7ff7887
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-10-04 23:20:46 -0300

    flexbox contenedor de articulos de noticias

--- src/assets/components/Noticias.jsx
+++ src/assets/components/Noticias.jsx
@@ -3,7 +3,7 @@ function Noticias() {
         <>
             <section className="text-white bg-emerald-500 py-10 flex flex-col items-center">
                 <h3 className="text-2xl lg:text-4xl font-bold uppercase">Noticias y eventos</h3>
-                <div>
+                <div className=" mx-10 flex justify-around">
                     <article>
                         <h6>13 septiembre 2023</h6>
                         <h4>Lorem ipsum dolor sit.</h4>
